581614dcea7c919cb818b3fe2cda46f49808828f
loeken <loeken@internetz.me>
2022-11-26 02:44:34 +0100

    base_app removed from charts folder

--- newchart.sh
+++ newchart.sh
@@ -3,8 +3,8 @@
 # $2 : chartversion
 # $3 : appversion
 mkdir charts/$1 -p
-cp charts/example/templates -r charts/$1/
-cp charts/example/Chart.yaml charts/$1/Chart.yaml
+cp base_app/templates -r charts/$1/
+cp base_app/Chart.yaml charts/$1/Chart.yaml
 sed -i "s/chartname/$1/g" charts/$1/Chart.yaml
 sed -i "s/chartversion/$2/g" charts/$1/Chart.yaml
 sed -i "s/appversion/$3/g" charts/$1/Chart.yaml
